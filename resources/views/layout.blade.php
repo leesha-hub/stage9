887d82c05b9f3137b0bc702d81ea199cc22eaefc
leesha-hub <leesha1223@naver.com>
2025-07-03 19:34:12 +0900

    css, js 분리

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,15 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Laravel 페이지</title>
+
+    {{-- 페이지별 CSS --}}
+    @yield('pageCss')
 </head>
 
 <body>
     @yield('content')
+
+    {{-- 페이지별 JS --}}
+    @yield('pageJs')
 </body>
 
 </html>
